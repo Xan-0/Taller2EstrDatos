5eb72450c8ff1d5d540f1c9624e17c7272f51a33
Xan-0 <johanalexis650@gmail.com>
2025-10-24 22:45:21 -0300

    Refactor main.cpp with SparseMatrix tests
    Se puede configurar con toma de muestras y números en las matrices aleatorios entre un rango.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,45 @@
-#include "SparseMatrix.h"
+#include "include\SparseMatrix.h"
+#include <limits>
+#include <chrono>
+#include <cstdlib>
+using namespace chrono;
 
-//Se pueden hacer pruebas aquí con la implementación de la Matriz
-int main(){
+//Ejemplo: caso con 50 datos, 100% densidad
+void muestra(int num){
+    srand(time(NULL));
     SparseMatrix *m1 = new SparseMatrix();
     SparseMatrix *m2 = new SparseMatrix();
     
-    
-    for(int i = 1; i<=10; i++){
-        if(i%2 == 0){
-            for(int j = 1; j<=5; j++){
-                m1 -> add(1,i,j);
-            }
+    for(int i = 10; i>=1; i--){
+        for(int j = 5; j>=1; j--){
+            //Un número entre 1 y 10
+            m1 -> add(1 + rand() % 10,i,j);
         }
     }
     
-    for(int i = 1; i<=5; i++){
-        for(int j = 1; j<=10; j++){
-            if(j%2 ==  0) m2 -> add(1,i,j);
+    cout << "__Muestra numero " << num << "__" << endl;
+    auto inicio = high_resolution_clock::now();
+    
+    m1 -> add(1,11,1);
+    auto fin = high_resolution_clock::now();
+    auto duracion = duration_cast<nanoseconds>(fin - inicio);
+    
+    cout << "Tiempo de ejecucion de add() en m1 -> (11,1): " << duracion.count() << " nanosegundos" << endl << endl;
+    
+    m1 -> remove(11,1);
+    
+    inicio = high_resolution_clock::now();
+    
+    m1 -> get(10,5);
+    
+    fin = high_resolution_clock::now();
+    duracion = duration_cast<nanoseconds>(fin - inicio);
+    
+    cout << "Tiempo de ejecucion de get() en m1 -> (10,5): " << duracion.count() << " nanosegundos" << endl << endl;
+    
+    for(int i = 5; i>=1; i--){
+        for(int j = 10; j>=1; j--){
+            m2 -> add(1 + rand() % 10,i,j);
         }
     }
     
@@ -39,8 +62,17 @@ int main(){
     cout<< endl;
     m2 -> printStoredValues();
     cout<< endl;
+    cout<< endl;
+    
+    inicio = high_resolution_clock::now();
     
     SparseMatrix *m3 = m1 -> multiply(m2);
+    
+    fin = high_resolution_clock::now();
+    duracion = duration_cast<microseconds>(fin - inicio);
+    
+    cout << "Tiempo de ejecucion de multiply() (m1 x m2): " << duracion.count() / 1000 << " milisegundos" << endl;
+    cout << endl;
     
     cout<< endl;
     cout<< "Matriz: m3" << endl;
@@ -61,6 +93,14 @@ int main(){
     m1 = nullptr;
     m2 = nullptr;
     m3 = nullptr;
+}
+
+//Aquí se pueden tomar muestras de pruebas con la implementación de la Matriz
+int main(){
+    int muestras = 5
+    for(int i = 1; i<=muestras; i++){
+        muestra(i);
+    }
     
     return 0;
 }
